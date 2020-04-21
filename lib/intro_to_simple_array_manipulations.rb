affc3d17347fb2666546e24bf6bec36f0fb38d3c
Samuel Lesser <ineedmoreplates@gmail.com>
2020-04-21 17:09:39 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+
+def using_push(array,string)
+var = array.push(string)  
+var
+end
+
+def using_unshift(array,string)
+var = array.unshift(string)
+var
+end
+
+def using_pop(array)
+var = array.pop
+var
+end
+
+def pop_with_args(array)
+var = array.pop(2)
+var
+end
+
+def using_shift(array)
+var= array.shift
+var
+end
+
+def shift_with_args(array)
+var= array.shift(2)
+var
+end
